fda8e0cd98b0257c0ea711c4277d9fd930d91a12
Ofrty <24553692+Ofrty@users.noreply.github.com>
2019-06-04 22:18:09 -0400

    java nonsense
    tried to do...something?

--- projects/kirkhamj/FinalProject/URLValidatorInCorrect/src/UrlValPackage/UrlValidator.java
+++ projects/kirkhamj/FinalProject/URLValidatorInCorrect/src/UrlValPackage/UrlValidator.java
@@ -1,4 +1,4 @@
-/*
+package UrlValPackage;/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -18,7 +18,6 @@
 import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
