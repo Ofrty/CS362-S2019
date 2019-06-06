a2279a3d7c1330658755359f9f26501d36d50fcf
glarff <46346564+glarff@users.noreply.github.com>
2019-06-06 19:28:03 -0400

    first unit test

--- projects/kirkhamj/FinalProject/URLValidatorInCorrect_java_learning/test/dumTest.java
+++ projects/kirkhamj/FinalProject/URLValidatorInCorrect_java_learning/test/dumTest.java
@@ -23,4 +23,34 @@ public class dumTest {
         assertTrue(dumUrlVal.isValid("http://www.google.com"));
     }
 
+    @Test
+    public void schemaTests()
+    {
+        UrlValidator smrtUrlVal = new UrlValidator();
+
+        // Positive Tests
+        assertTrue(smrtUrlVal.isValid("http://www.google.com"));        // normal URL
+        assertTrue(smrtUrlVal.isValid("hTTp://www.google.com"));        // verify different case
+        assertTrue(smrtUrlVal.isValid("ftp://www.google.com"));         // Not HTTP or HTTPS
+
+        // Negative Tests
+        assertFalse(smrtUrlVal.isValid("httpz://www.google.com"));      // invalid prefix
+        assertFalse(smrtUrlVal.isValid("hTTp:/www.google.com"));        // missing forward slash
+        assertFalse(smrtUrlVal.isValid(" hTTp://www.google.com"));      // space before url
+
+        // Boundary Tests
+        assertFalse(smrtUrlVal.isValid("&#102;&#116;&#112;&#58;&#47;&#47;//www.google.com"));  // Unicode
+        assertFalse(smrtUrlVal.isValid("http://"));                     // no entry
+
+    }
+
+    @Test
+    public void authorityTests()
+    {
+        UrlValidator smrtUrlVal = new UrlValidator();
+
+
+    }
+
+
 }
